f037b7ceafa436914c6a50a6d41ae6a7bc81bc99
Victor-Suander <victors.empresa@gmail.com>
2025-07-18 20:43:17 -0300

    Project create

---
+++ src/main/java/com/map2work/map2work/Map2workApplication.java
@@ -0,0 +1,13 @@
+package com.map2work.map2work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Map2workApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Map2workApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/map2work/map2work/Map2workApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map2work.map2work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Map2workApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
